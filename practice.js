e817bd6306ad6896d10f3aa2813fa94f1a9551f0
Dmytro2V <op2vin@gmail.com>
2022-09-20 04:02:05 +0300

    done

--- practice.js
+++ practice.js
@@ -1,47 +1,26 @@
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  return getBiggerSmaller(arguments) * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return getBiggerSmaller(arguments) / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  return `I ate ${getBiggerSmaller(arguments)} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  return `I adopted a dog that weighs ${getBiggerSmaller(arguments, false)} pounds.`;
 }
 
+function getBiggerSmaller(Arguments, biggerButSmallerIfFalse = true) {  
+  let args = Object.values(Arguments)  // Object to array
+  console.log(args, Math.max(...args));
+  if (biggerButSmallerIfFalse) return Math.max(...args);
+  console.log();
+  return Math.min(...args);
+}
 
 /**************************************************************************/
 /* DO NOT CHANGE THE CODE BELOW */
